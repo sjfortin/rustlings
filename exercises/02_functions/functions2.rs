61f8cb6f129caa27af6b5ca16e7dd474fd6b8278
sjfortin <sam.j.fortin@gmail.com>
2024-02-26 20:38:08 -0600

    functions fin

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -3,13 +3,12 @@
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me(3);
 }
 
-fn call_me(num:) {
+// i32 represents a 32-bit signed integer, while u32 represents a 32-bit unsigned integer. The difference is that i32 can hold both positive and negative numbers, while u32 can only hold non-negative numbers.
+fn call_me(num: u32) { 
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
 }
 
+
+// In Rust, the last expression in a function is implicitly returned. By adding a semicolon at the end of num * num;, you turn it into a statement, and the value is not returned. Removing the semicolon makes it an expression, and the result is returned.
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
