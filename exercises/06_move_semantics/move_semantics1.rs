b9a056d090276d82165d240929fe9de25f0d38ce
sjfortin <sam.j.fortin@gmail.com>
2024-03-05 18:45:02 -0600

    move semantics fin

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -3,19 +3,17 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
-    let vec0 = vec![22, 44, 66];
+    let vec0 = vec![22, 44, 66]; // vec0 comes into scope and the exclamation points means it's not going to be moved
 
-    let vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(vec0); 
 
-    assert_eq!(vec1, vec![22, 44, 66, 88]);
+    assert_eq!(vec1, vec![22, 44, 66, 88]); // 
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
